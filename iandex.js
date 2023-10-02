5bb20a864b697698f06f375b3b184b4f89287934
ruslanchikm <ruslanmatran@gmail.com>
2023-10-02 19:21:24 +0300

    inca o modificare

--- iandex.js
+++ iandex.js
@@ -8,3 +8,14 @@ function adunare (num1, num2) {
 }
 var rezultatFinal = adunare(5, 3);
 console.log(rezultatFinal);
+
+// exercitiul cu majuscula
+console.log("\nEXERCITIUL 2")
+
+var text = "hhvblbvbwbJLHHVLJVJLHVvwbdcvkbwckbj";
+
+var textMajuscule = text.toUpperCase();
+ 
+console.log(textMajuscule);
+var textMinuscule = textMajuscule.toLowerCase();
+console.log(textMinuscule);
